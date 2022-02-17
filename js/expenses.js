e827669aca9d16504aa11022cab6669281f4677d
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-17 17:02:43 +0600

    without function done

--- js/expenses.js
+++ js/expenses.js
@@ -26,17 +26,62 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
 document.getElementById('save-btn').addEventListener('click', function () {
 
+    //1-saving input  2-save diplay  3-income value  4-percetag 5-save display change
     const saveMoneyInput = document.getElementById('save-money');
     const saveMoney = parseFloat(saveMoneyInput.value);
 
     const displaySaveAmount = document.getElementById('save-display');
 
+    //income input and its value
     const incomeInput = document.getElementById('income-input');
     const incomeAmount = parseFloat(incomeInput.value);
 
     const savingAmount = (incomeAmount / 100) * saveMoney;
 
     displaySaveAmount.innerText = savingAmount;
+
+    //1-remaining balance inputtext 2-Main balance value 3-saving amount parseFloat 4- balance minus saving ammount  4- remainging balance inputtext change
+
+
+
+    //2
+    //previous thing to get blance 
+    //income - total expenses
+    const foodInput = document.getElementById('food-cost');
+    const foodCost = parseFloat(foodInput.value);
+
+    const rentInput = document.getElementById('rent-cost');
+    const rentCost = parseFloat(rentInput.value);
+
+    const clothInput = document.getElementById('cloth-cost');
+    const clothCost = parseFloat(clothInput.value);
+
+    const expenseInput = document.getElementById('expense-amount');
+
+    const expenseAmount = foodCost + rentCost + clothCost;
+
+    expenseInput.innerText = expenseAmount;
+
+    //income upore ache
+
+    const totalBalance = incomeAmount - expenseAmount;
+
+    const balanceInput = document.getElementById('balance-amount');
+    balanceInput.innerText = totalBalance;
+
+    //3
+
+    //4
+    const remaingingBalance = totalBalance - savingAmount;
+
+
+    const remaingingInput = document.getElementById('remaining-amount');
+    remaingingInput.innerText = remaingingBalance;
+
+
+
+
+
 
 
 
