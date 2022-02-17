d0003eed9569011639f1411a6b3c8d5d3c59f9cc
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-17 20:52:20 +0600

    some error handled

--- js/expenses.js
+++ js/expenses.js
@@ -1,30 +1,40 @@
 function getInput(costId) {
+
     const productInput = document.getElementById(costId);
     const productCost = parseFloat(productInput.value);
-    return productCost
+
+    if (!isNaN(productCost) && productCost > -1) {
+        document.getElementById('calculate-btn').disabled = false;
+        return productCost;
+    }
+    else {
+        alert('positve number');
+    }
+
 }
 
 function calculateCost() {
 
-
+    const incomeAmount = getInput('income-input');
     const foodCost = getInput('food-cost');
     const rentCost = getInput('rent-cost');
     const clothCost = getInput('cloth-cost')
 
     const expenseInput = document.getElementById('expense-amount');
-
     const expenseAmount = foodCost + rentCost + clothCost;
-    expenseInput.innerText = expenseAmount;
 
-    const incomeInput = document.getElementById('income-input');
-    const incomeAmount = parseFloat(incomeInput.value);
-
-    const totalBalance = incomeAmount - expenseAmount;
-
-    const balanceInput = document.getElementById('balance-amount');
-    balanceInput.innerText = totalBalance;
-
-    return totalBalance;
+    if (incomeAmount > expenseAmount) {
+        expenseInput.innerText = expenseAmount;
+        const totalBalance = incomeAmount - expenseAmount;
+        const balanceInput = document.getElementById('balance-amount');
+        balanceInput.innerText = totalBalance;
+        return totalBalance;
+    }
+    else {
+        const errorWarn = document.getElementById('error-warn');
+        errorWarn.innerText = 'error';
+        console.log('error');
+    }
 
 }
 
@@ -35,8 +45,6 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 })
 
 document.getElementById('save-btn').addEventListener('click', function () {
-
-
 
     //1-saving input  2-save diplay  3-income value  4-percetag 5-save display change
 
