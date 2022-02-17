f2a8fe9a53e91485a21d0eef3f29942b1d9cca32
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-17 22:01:59 +0600

    error almost done

--- js/expenses.js
+++ js/expenses.js
@@ -1,14 +1,16 @@
 function getInput(costId) {
 
     const productInput = document.getElementById(costId);
-    const productCost = parseFloat(productInput.value);
+    const productText = productInput.value;
+    const productCost = parseFloat(productText.value);
 
     if (!isNaN(productCost) && productCost > -1) {
-        document.getElementById('calculate-btn').disabled = false;
+
         return productCost;
     }
     else {
         alert('positve number');
+
     }
 
 }
@@ -23,7 +25,7 @@ function calculateCost() {
     const expenseInput = document.getElementById('expense-amount');
     const expenseAmount = foodCost + rentCost + clothCost;
 
-    if (incomeAmount > expenseAmount) {
+    if (typeof incomeAmount == 'number' && typeof expenseAmount == 'number' && incomeAmount > expenseAmount) {
         expenseInput.innerText = expenseAmount;
         const totalBalance = incomeAmount - expenseAmount;
         const balanceInput = document.getElementById('balance-amount');
@@ -32,8 +34,7 @@ function calculateCost() {
     }
     else {
         const errorWarn = document.getElementById('error-warn');
-        errorWarn.innerText = 'error';
-        console.log('error');
+        errorWarn.innerText = 'Expense is greather then or equal to Income';
     }
 
 }
@@ -49,19 +50,27 @@ document.getElementById('save-btn').addEventListener('click', function () {
     //1-saving input  2-save diplay  3-income value  4-percetag 5-save display change
 
     const saveMoney = getInput('save-money')
-    const displaySaveAmount = document.getElementById('save-display');
+    // const displaySaveAmount = document.getElementById('save-display');
     //income input and its value
-
     const incomeAmount = getInput('income-input')
     const savingAmount = (incomeAmount / 100) * saveMoney;
-    displaySaveAmount.innerText = savingAmount;
-
-
     const totalBalance = calculateCost();
-    const remaingingBalance = totalBalance - savingAmount;
 
-    //change remaining balance text field
-    const remaingingInput = document.getElementById('remaining-amount');
-    remaingingInput.innerText = remaingingBalance;
+    if (totalBalance > savingAmount) {
+
+
+        const displaySaveAmount = document.getElementById('save-display');
+        displaySaveAmount.innerText = savingAmount;
+        const remaingingBalance = totalBalance - savingAmount;
+        const remaingingInput = document.getElementById('remaining-amount');
+        remaingingInput.innerText = remaingingBalance;
+    }
+    else {
+        const errorWarn = document.getElementById('error-warn');
+        errorWarn.innerText = 'Not enough money for savings';
+    }
+
+
+
 
 })
