9375138af381b32053ef0a10ca006a82b436db5a
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-02-17 19:25:48 +0600

    function updat

--- js/expenses.js
+++ js/expenses.js
@@ -4,14 +4,12 @@ function getInput(costId) {
     return productCost
 }
 
-document.getElementById('calculate-btn').addEventListener('click', function () {
+function calculateCost() {
 
 
     const foodCost = getInput('food-cost');
     const rentCost = getInput('rent-cost');
     const clothCost = getInput('cloth-cost')
-
-
 
     const expenseInput = document.getElementById('expense-amount');
 
@@ -25,61 +23,36 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     const balanceInput = document.getElementById('balance-amount');
     balanceInput.innerText = totalBalance;
+
+    return totalBalance;
+
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    calculateCost();
+
 })
 
 document.getElementById('save-btn').addEventListener('click', function () {
 
+
+
     //1-saving input  2-save diplay  3-income value  4-percetag 5-save display change
 
     const saveMoney = getInput('save-money')
-
     const displaySaveAmount = document.getElementById('save-display');
-
     //income input and its value
-    // const incomeInput = document.getElementById('income-input');
-    // const incomeAmount = parseFloat(incomeInput.value);
 
     const incomeAmount = getInput('income-input')
-
     const savingAmount = (incomeAmount / 100) * saveMoney;
-
     displaySaveAmount.innerText = savingAmount;
 
-    //1-remaining balance inputtext 2-Main balance value 3-saving amount parseFloat 4- balance minus saving ammount  4- remainging balance inputtext change
 
-
-
-    //2
-    //previous thing to get blance 
-    //income - total expenses
-    const foodInput = document.getElementById('food-cost');
-    const foodCost = parseFloat(foodInput.value);
-
-    const rentInput = document.getElementById('rent-cost');
-    const rentCost = parseFloat(rentInput.value);
-
-    const clothInput = document.getElementById('cloth-cost');
-    const clothCost = parseFloat(clothInput.value);
-
-    const expenseInput = document.getElementById('expense-amount');
-
-    const expenseAmount = foodCost + rentCost + clothCost;
-
-    expenseInput.innerText = expenseAmount;
-
-    //income upore ache
-
-    const totalBalance = incomeAmount - expenseAmount;
-
-    const balanceInput = document.getElementById('balance-amount');
-    balanceInput.innerText = totalBalance;
-
-    //3
-
-    //4
+    const totalBalance = calculateCost();
     const remaingingBalance = totalBalance - savingAmount;
 
-
+    //change remaining balance text field
     const remaingingInput = document.getElementById('remaining-amount');
     remaingingInput.innerText = remaingingBalance;
 
